46a6866556c72411cce93c524e07fe8d483f50e7
Hariharan Sandhrasekar <hariharan.sandhrasekar@gmail.com>
2026-03-17 13:02:03 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzAPQBVSMtqpR0UNlCtx_dlQKkOqH7-LI",
-  authDomain: "harryconnectvalidation.firebaseapp.com",
-  projectId: "harryconnectvalidation",
-  storageBucket: "harryconnectvalidation.firebasestorage.app",
-  messagingSenderId: "650421984578",
-  appId: "1:650421984578:web:543cff21f60c0bb451bcdb",
-  measurementId: "G-XXNQGQX20J"
+  apiKey: "your_api_key",
+  authDomain: "your_auth_domain",
+  projectId: "your_project_id",
+  storageBucket: "your_storage_bucket",
+  messagingSenderId: "your_messaging_sender_id",
+  appId: "your_app_id",
+  measurementId: "your_mesurement_id"
 };
 
 const app = initializeApp(firebaseConfig);
